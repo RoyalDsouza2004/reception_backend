84ca52718e1cf10b7c3d7484c73481ed247a6c61
Royal dsouza <royaldsouza2004@gmail.com>
2024-03-07 19:46:08 +0530

    changes in env

--- data/database.js
+++ data/database.js
@@ -7,12 +7,12 @@ config({
 
 const connection = mysql.createPool({
       host: process.env.DATABASE_HOST,
-      port: process.env.PORT,
+      port: process.env.DB_PORT,
       user: process.env.DATABASE_USER,
       password: process.env.PASSWORD,
       database: process.env.DATABASE_NAME,
 }).promise();
 
-setTimeout(()=> console.log(`Database connected in ${process.env.DATABASE_HOST}` ) , 1);
+setTimeout(()=> console.log(`Database connected in ${process.env.DATABASE_HOST} and ` ) , 1);
 
 export {connection};

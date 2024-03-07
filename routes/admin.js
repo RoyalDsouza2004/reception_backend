bbbe6d3dfceff3c5875d04dd3486d2a0abf7b582
Royal dsouza <royaldsouza2004@gmail.com>
2024-03-07 21:23:33 +0530

    changes

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import { register ,login, logout , profile } from '../controllers/admin.js';
-import { isAuthenticated } from '../middlewares/auth.js';
 
 const router = express.Router();
 
 router.post("/register" ,register);
 
-router.get("/profile",isAuthenticated, profile);
+router.get("/profile", profile);
 
 router.post("/login" , login);
 
